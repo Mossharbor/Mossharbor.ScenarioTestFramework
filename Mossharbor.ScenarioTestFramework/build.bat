c48e8561b7ee5528a305e69ec8b9b0cb4af8224d
Matthew Johnson <mattj@mossharbor.com>
2020-12-28 13:57:32 -0800

    Updated nuspec and build to version 1.0.5

--- Mossharbor.ScenarioTestFramework/build.bat
+++ Mossharbor.ScenarioTestFramework/build.bat
@@ -1,1 +1,3 @@
-msbuild /t:pack /p:NuspecFile=Package.nuspec Mossharbor.ScenarioTestFramework.csproj 
+REM msbuild /t:pack /p:NuspecFile=Package.nuspec Mossharbor.ScenarioTestFramework.csproj 
+dotnet build Mossharbor.ScenarioTestFramework.csproj
+dotnet pack -p:NuspecFile=.\Package.nuspec
